60c163f9d97b13a711516c3a8cdca0e5b7732aad
adipinhas <adi9247@gmail.com>
2020-02-20 22:03:31 +0200

    Updates

---
+++ precache-manifest.a662dfbae8f2a5d95c1a219f45f4326a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198ee093569de2d22436ab6ff56a84f7",
+    "url": "/Hooks-Todos/index.html"
+  },
+  {
+    "revision": "01c0b390f152ffa6e433",
+    "url": "/Hooks-Todos/static/css/main.752b6524.chunk.css"
+  },
+  {
+    "revision": "3af73e93ea5ec313886b",
+    "url": "/Hooks-Todos/static/js/2.bdef8bef.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Hooks-Todos/static/js/2.bdef8bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c0b390f152ffa6e433",
+    "url": "/Hooks-Todos/static/js/main.9bcc3c1f.chunk.js"
+  },
+  {
+    "revision": "3700a6583390c9c932d4",
+    "url": "/Hooks-Todos/static/js/runtime-main.5ab9c946.js"
+  }
+]);
